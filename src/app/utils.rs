85955a1726255b2697de7c3b4b16e8c3b0f4aad4
nekluu <mustafaxuglen@gmail.com>
2026-02-20 00:21:10 +0300

    refactor remove useless function

--- src/app/utils.rs
+++ src/app/utils.rs
@@ -2,18 +2,7 @@ use iced::futures::{Stream, stream};
 use iced::Subscription;
 use crate::app::Message;
 
-pub fn stream_logic() -> impl Stream<Item = usize> {
-    let counter = 0;
-    stream::unfold(counter, move |mut state| async move {
-        if state >= 1000 {
-            None
-        } else {
-            state += 1;
-            tokio::time::sleep(std::time::Duration::from_millis(50)).await;
-            Some((state, state))
-        }
-    })
-}
+
 pub fn subscription() -> Subscription<Message> {
     iced::event::listen().map(Message::Event)
 }
